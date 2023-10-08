1e080185229989916f624b24744e1ba5acc49609
arozhdan <halala30@gmail.com>
2023-10-08 13:13:34 +0200

    [deploy] fix pm2 script

--- pm2_runner.sh
+++ pm2_runner.sh
@@ -1,6 +1,6 @@
 if ! type pm2 > /dev/null
 then
-    sudo npm install -g pm2 && pm2 start npm --name app -- run serve
+    sudo npm install -g pm2 && pm2 start . npm --name app -- run serve
 else
-    sudo pm2 restart app || sudo pm2 start npm --name app -- run serve
+    pm2 restart app || pm2 start npm --name app -- run serve
 fi
